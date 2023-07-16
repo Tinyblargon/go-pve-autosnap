f20a044baf13c0a071a4f1ddff0adabbb8541a12
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-07-16 11:41:12 +0000

    feat: add `Execute()`
    Runs the provided function on all guests that match the filter.

--- cmd/autosnap/autosnap.go
+++ cmd/autosnap/autosnap.go
@@ -1,10 +1,22 @@
 package autosnap
 
 import (
+	"fmt"
 	"go-pve-autosnap/internal/filter"
 
 	pxAPI "github.com/Telmate/proxmox-api-go/proxmox"
+	"golang.org/x/exp/slices"
 )
+
+// check if a guest is already in the list
+func completed(guest filter.Guest, guests []guestMinimal) bool {
+	for i := range guests {
+		if guest.Id == guests[i].Id && guest.Type == guests[i].Type {
+			return true
+		}
+	}
+	return false
+}
 
 // convert a list of proxmox guests to a list of guests for the filter
 // the difference is that the filter.Guest type is way smaller helping with memory usage
@@ -32,4 +44,102 @@ func convertToGuestList(guests []pxAPI.GuestResource) []filter.Guest {
 		}
 	}
 	return guestList
+}
+
+// run the provided function on all guests that match the filter.
+func Execute(client *pxAPI.Client, filterObj *filter.FilterSteps, function func(client *pxAPI.Client, vmr *pxAPI.VmRef) error) error {
+	if filterObj == nil {
+		return fmt.Errorf("filter is nil")
+	}
+	trackedGuests := guests{
+		Filtered:  make(map[uint]guestNoID),
+		Completed: make([]guestMinimal, 0),
+	}
+
+	var subroutineSucceeded bool
+	for !subroutineSucceeded {
+		guestList, err := newGuestList(client)
+		if err != nil {
+			return err
+		}
+		subroutineSucceeded = executeSubroutine(guestList, &trackedGuests, client, filterObj, function)
+	}
+	return nil
+}
+
+// subroutine of Execute to extract the testable code.
+func executeSubroutine(guestList []filter.Guest, trackedGuests *guests, client *pxAPI.Client, filterObj *filter.FilterSteps, function func(client *pxAPI.Client, vmr *pxAPI.VmRef) error) bool {
+	for _, e := range guestList {
+		// check if a snapshot has been made of the guest during this run.
+		if completed(e, trackedGuests.Completed) {
+			continue
+		}
+		// check if guest has been filtered this run.
+		// faster than running the filter on every time.
+		if filtered(e, trackedGuests.Filtered) {
+			continue
+		}
+		if filterObj.Apply(e) {
+			vmr := pxAPI.NewVmRef(int(e.Id))
+			vmr.SetNode(e.Node)
+			vmr.SetVmType(string(e.Type))
+			err := function(client, vmr)
+			if err != nil {
+				// if err guest list is out of date, obtain a new one.
+				return false
+			}
+			trackedGuests.Completed = append(trackedGuests.Completed, guestMinimal{Id: e.Id, Type: e.Type})
+			continue
+		}
+		slices.Sort[string](e.Tags)
+		trackedGuests.Filtered[e.Id] = guestNoID{
+			Name: e.Name,
+			Node: e.Node,
+			Pool: e.Pool,
+			Tags: e.Tags,
+			Type: e.Type,
+		}
+	}
+	return true
+}
+
+// check if a guest is already in the map
+// the tags of the guests map[uint]guestNoID have to be sorted
+func filtered(guest filter.Guest, guests map[uint]guestNoID) bool {
+	if value, ok := guests[guest.Id]; ok {
+		if value.Type == guest.Type && value.Name == guest.Name && value.Node == guest.Node && value.Pool == guest.Pool {
+			slices.Sort[string](guest.Tags)
+			if slices.Equal(value.Tags, guest.Tags) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// obtain a new guest list from the proxmox api
+func newGuestList(client *pxAPI.Client) ([]filter.Guest, error) {
+	rawGuestList, err := pxAPI.ListGuests(client)
+	if err != nil {
+		return nil, err
+	}
+	return convertToGuestList(rawGuestList), nil
+}
+
+type guestMinimal struct {
+	Id   uint
+	Type pxAPI.GuestType
+}
+
+type guestNoID struct {
+	Name string
+	Node string
+	Pool string
+	Tags []string
+	Type pxAPI.GuestType
+}
+
+type guests struct {
+	Filtered  map[uint]guestNoID
+	Completed []guestMinimal
 }
--- cmd/autosnap/autosnap_test.go
+++ cmd/autosnap/autosnap_test.go
@@ -1,12 +1,51 @@
 package autosnap
 
 import (
+	"errors"
 	"go-pve-autosnap/internal/filter"
 	"testing"
 
 	pxAPI "github.com/Telmate/proxmox-api-go/proxmox"
 	"github.com/stretchr/testify/require"
 )
+
+func Test_completed(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  filter.Guest
+		inList []guestMinimal
+		output bool
+	}{
+		{name: "Empty list",
+			input:  filter.Guest{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1"},
+			inList: []guestMinimal{},
+			output: false,
+		},
+		{name: "In list",
+			input: filter.Guest{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1"},
+			inList: []guestMinimal{
+				{Id: 101, Type: pxAPI.GuestQemu},
+				{Id: 102, Type: pxAPI.GuestLXC},
+				{Id: 100, Type: pxAPI.GuestLXC},
+			},
+			output: true,
+		},
+		{name: "Not in list",
+			input: filter.Guest{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1"},
+			inList: []guestMinimal{
+				{Id: 101, Type: pxAPI.GuestQemu},
+				{Id: 102, Type: pxAPI.GuestLXC},
+				{Id: 103, Type: pxAPI.GuestLXC},
+			},
+			output: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(*testing.T) {
+			require.Equal(t, test.output, completed(test.input, test.inList))
+		})
+	}
+}
 
 func Test_convertToGuestList(t *testing.T) {
 	tests := []struct {
@@ -42,6 +81,155 @@ func Test_convertToGuestList(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(*testing.T) {
 			require.Equal(t, test.output, convertToGuestList(test.input))
+		})
+	}
+}
+
+func Test_executeSubroutine(t *testing.T) {
+	tests := []struct {
+		name          string
+		guests        []filter.Guest
+		filterObj     *filter.FilterSteps
+		trackedGuests *guests
+		function      func(client *pxAPI.Client, vmr *pxAPI.VmRef) error
+		output        bool
+		outputTracked *guests
+	}{
+		{name: "add to trackedGuests.Completed",
+			guests:    []filter.Guest{{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1"}},
+			filterObj: &filter.FilterSteps{All: true},
+			trackedGuests: &guests{
+				Filtered:  make(map[uint]guestNoID),
+				Completed: []guestMinimal{{Id: 200, Type: pxAPI.GuestQemu}},
+			},
+			function: func(client *pxAPI.Client, vmr *pxAPI.VmRef) error {
+				return nil
+			},
+			output: true,
+			outputTracked: &guests{
+				Filtered: make(map[uint]guestNoID),
+				Completed: []guestMinimal{
+					{Id: 200, Type: pxAPI.GuestQemu},
+					{Id: 100, Type: pxAPI.GuestLXC},
+				},
+			},
+		},
+		{name: "add to trackedGuests.Filtered",
+			guests:    []filter.Guest{{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1", Tags: []string{"no-snapshot", "automated"}}},
+			filterObj: &filter.FilterSteps{All: false},
+			trackedGuests: &guests{
+				Filtered:  map[uint]guestNoID{200: {Name: "test", Type: pxAPI.GuestQemu, Node: "pve"}},
+				Completed: make([]guestMinimal, 0),
+			},
+			function: func(client *pxAPI.Client, vmr *pxAPI.VmRef) error {
+				return nil
+			},
+			output: true,
+			outputTracked: &guests{
+				Filtered: map[uint]guestNoID{
+					// tags have to be sorted
+					100: {Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1", Tags: []string{"automated", "no-snapshot"}},
+					200: {Name: "test", Type: pxAPI.GuestQemu, Node: "pve"},
+				},
+				Completed: make([]guestMinimal, 0),
+			},
+		},
+		{name: "return false on error",
+			guests:    []filter.Guest{{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1"}},
+			filterObj: &filter.FilterSteps{All: true},
+			trackedGuests: &guests{
+				Filtered:  map[uint]guestNoID{200: {Name: "test", Type: pxAPI.GuestQemu, Node: "pve"}},
+				Completed: []guestMinimal{{Id: 500, Type: pxAPI.GuestQemu}},
+			},
+			function: func(client *pxAPI.Client, vmr *pxAPI.VmRef) error {
+				return errors.New("test error")
+			},
+			output: false,
+			outputTracked: &guests{
+				Filtered:  map[uint]guestNoID{200: {Name: "test", Type: pxAPI.GuestQemu, Node: "pve"}},
+				Completed: []guestMinimal{{Id: 500, Type: pxAPI.GuestQemu}},
+			},
+		},
+		{name: "skip trackedGuests.Completed",
+			guests:    []filter.Guest{{Id: 100, Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve"}},
+			filterObj: &filter.FilterSteps{All: false},
+			trackedGuests: &guests{
+				Filtered:  make(map[uint]guestNoID),
+				Completed: []guestMinimal{{Id: 100, Type: pxAPI.GuestQemu}},
+			},
+			function: func(client *pxAPI.Client, vmr *pxAPI.VmRef) error {
+				require.FailNow(t, "function should not be called")
+				return nil
+			},
+			output: true,
+		},
+		{name: "skip trackedGuests.Filtered",
+			guests:    []filter.Guest{{Id: 100, Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve"}},
+			filterObj: &filter.FilterSteps{All: false},
+			trackedGuests: &guests{
+				Filtered:  map[uint]guestNoID{100: {Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve"}},
+				Completed: make([]guestMinimal, 0),
+			},
+			function: func(client *pxAPI.Client, vmr *pxAPI.VmRef) error {
+				require.FailNow(t, "function should not be called")
+				return nil
+			},
+			output: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(*testing.T) {
+			require.Equal(t, test.output, executeSubroutine(test.guests, test.trackedGuests, nil, test.filterObj, test.function))
+			if test.outputTracked != nil {
+				require.Equal(t, test.outputTracked, test.trackedGuests)
+			}
+		})
+	}
+}
+
+func Test_filtered(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  filter.Guest
+		inMap  map[uint]guestNoID
+		output bool
+	}{
+		{name: "Empty map",
+			input:  filter.Guest{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1"},
+			inMap:  make(map[uint]guestNoID),
+			output: false,
+		},
+		{name: "In map",
+			input: filter.Guest{Id: 100, Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve1", Tags: []string{"no-snapshot", "automated"}},
+			inMap: map[uint]guestNoID{
+				101: {Name: "test", Type: pxAPI.GuestLXC, Node: "pve"},
+				100: {Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve1", Tags: []string{"automated", "no-snapshot"}},
+				200: {Name: "third-test", Type: pxAPI.GuestLXC, Node: "test", Tags: []string{"no-snapshot"}},
+			},
+			output: true,
+		},
+		{name: "Map tags not in order",
+			input: filter.Guest{Id: 100, Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve1", Tags: []string{"no-snapshot", "automated"}},
+			inMap: map[uint]guestNoID{
+				101: {Name: "test", Type: pxAPI.GuestLXC, Node: "pve"},
+				100: {Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve1", Tags: []string{"no-snapshot", "automated"}},
+				200: {Name: "third-test", Type: pxAPI.GuestLXC, Node: "test", Tags: []string{"no-snapshot"}},
+			},
+			output: false,
+		},
+		{name: "Not in map",
+			input: filter.Guest{Id: 100, Name: "abcde", Type: pxAPI.GuestLXC, Node: "pve1"},
+			inMap: map[uint]guestNoID{
+				101: {Name: "test", Type: pxAPI.GuestLXC, Node: "pve"},
+				100: {Name: "abcde", Type: pxAPI.GuestQemu, Node: "pve1", Tags: []string{"automated", "no-snapshot"}},
+				200: {Name: "third-test", Type: pxAPI.GuestLXC, Node: "test", Tags: []string{"no-snapshot"}},
+			},
+			output: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(*testing.T) {
+			require.Equal(t, test.output, filtered(test.input, test.inMap))
 		})
 	}
 }
